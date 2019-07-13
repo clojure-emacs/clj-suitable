69ceba9dbe4f1b172f1d193f037409fd2d85ce07
Robert Krahn <robert.krahn@gmail.com>
2019-07-13 15:50:18 +0200

    initial

---
+++ .dir-locals.el
@@ -0,0 +1,4 @@
+((nil . ((cider-default-cljs-repl . figwheel-main)
+	 (cider-preferred-build-tool . clojure-cli)
+	 (cider-clojure-cli-global-options . "-R:fig")
+	 (cider-figwheel-main-default-options . ":fig"))))
---
+++ src/cljs_object_completion/core.cljs
@@ -0,0 +1,5 @@
+(ns cljs-object-completion.core)
+
+(enable-console-print!)
+
+(prn "hello world!!!")
